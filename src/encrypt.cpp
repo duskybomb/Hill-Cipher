133549502f7336d84a4787182e5bb007d810fc7c
Harshit Joshi <harshit.joshi@outlook.com>
2017-11-03 01:12:38 +0530

    Odd case running

--- src/encrypt.cpp
+++ src/encrypt.cpp
@@ -10,42 +10,54 @@ void takeinput(int key[2][2])
 	//plaint = "aahaah";
 	cin.ignore(256,'\n');
 	getline(cin,plaint);
+	ofstream myfile;
+ 	myfile.open ("out.txt");
+ 	myfile<<"Key = \n";
+ 	for(int l=0;l<2;l++)
+ 	{
+ 		for(int ll=0;ll<2;ll++)
+ 			myfile <<key[l][ll]<<" ";
+ 		myfile<<"\n";
+ 	}
+ 	myfile<<"Plain Text = "<<plaint;
+  	myfile.close();
 	n = plaint.length();
-	if (n%2 == 0)
+	if (n%2 != 0)
 	{
-		int plaineven[2][n/2];
-		int k=0;
-		for (int i=0;i< n/2;i++)
-		{
-			for(int j=0;j<2;j++)
-			{
-				plaineven[j][i] = plaint[k];
-				//cout<<"xxx"<<plaineven[j][i]<<"xxx  ";
-				k++;
-			}
-		}
-
-		for(int m=0;m<n/2;m++)
-		{
-			for(int f=0;f<2;f++)
-			{
-				int mulmat = 0,temp = 0;
-				for(int g=0;g<2;g++)
-				{
-					//temp = 0;
-					temp = key[f][g] * plaineven[g][m]; 
-					mulmat += temp;
-					//cout<<temp<<"  "<<mulmat<<" ";
-				}
-				mulmat = (mulmat % 95) + 32;
-				//cout<<".."<<mulmat<<" ..  ";
-				char s = mulmat;
-				cout<<s<<"  ";
-			}
-
-		}
-
+		plaint.append("0");
 	}
+	int plaineven[2][n/2];
+	int k=0;
+	for (int i=0;i< n/2;i++)
+	{
+		for(int j=0;j<2;j++)
+		{
+			plaineven[j][i] = plaint[k];
+			//cout<<"xxx"<<plaineven[j][i]<<"xxx  ";
+			k++;
+		}
+	}
+
+	for(int m=0;m<n/2;m++)
+	{
+		for(int f=0;f<2;f++)
+		{
+			int mulmat = 0,temp = 0;
+			for(int g=0;g<2;g++)
+			{
+				//temp = 0;
+				temp = key[f][g] * plaineven[g][m]; 
+				mulmat += temp;
+				//cout<<temp<<"  "<<mulmat<<" ";
+			}
+			mulmat = (mulmat % 95) + 32;
+			//cout<<".."<<mulmat<<" ..  ";
+			char s = mulmat;
+			cout<<s<<"  ";
+		}
+	}
+
+	/*
 	else
 	{
 		for (int i=0;i< n/2 + 1;i++)
@@ -82,7 +94,7 @@ void takeinput(int key[2][2])
 
 		}
 		}
-	}
+	}*/
 	
 }
 
